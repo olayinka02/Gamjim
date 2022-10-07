40c618efc38bdbc3f3f7d4aab61ac203358a400a
Olayinka <64751867+olayinka02@users.noreply.github.com>
2022-10-07 15:15:02 +0100

    Gamjim

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+}
+
+
+
+setTimeout(() => {
+  const box = document.getElementById('loaddingbox');
+
+  
+  box.style.display = 'none';
+
+ 
+}, 4000); // 👈️ t
+
+
+
+
+
 
   $(document).ready(function () {
     $('#nav-icon3').click(function () {
       $(this).toggleClass('open');
     });
   });
-
 
  
